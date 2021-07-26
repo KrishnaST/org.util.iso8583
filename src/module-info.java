2f80e851d61eb6d9ee7dd042b0c98f3fa591defa
K <K@KPC>
2021-07-26 12:33:34 +0530

    added package org.util.iso8583.encoder to exports

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
 module org.util.iso8583 {
 
 	exports org.util.iso8583;
-	exports org.util.iso8583.format;
 	exports org.util.iso8583.api;
 	exports org.util.iso8583.util;
+	exports org.util.iso8583.format;
+	exports org.util.iso8583.encoder;
 
 	requires static transitive java.xml;
 }
